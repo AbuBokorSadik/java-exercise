67af0edaa6c8a381e54f9f331b2581a12ec55ceb
Abu Bokor Sadik <sadikabubokor@gmail.com>
2020-09-06 01:43:38 +0600

    HelloWorld/FlourPacker changed

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello Sadik");
 
-        int number = (10 + 5) + (2*10);
+        int number = (10 + 22) + (2*10);
         int number2 = 12;
         int number3 = number * 2;
         int total = number + number2 + number3;
