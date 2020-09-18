9caa4f9287256de0c15e068d199851afd6e4a955
Abu Bokor Sadik <sadikabubokor@gmail.com>
2020-09-18 23:31:19 +0600

    ArrayChallenge

---
+++ ArrayChallenge/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] array = getIntegers(5);
+        printArray(array);
+        array = sortIntegers(array).clone();
+        printArray(array);
+
+    }
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter values: ");
+        int[] array = new int[number];
+        for (int i=0;i<number;i++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array){
+        System.out.println("Print values: ");
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] sortIntegers(int[] array){
+        int temp;
+        for (int i=0;i<array.length-1;i++){
+            for (int j=0;j<array.length-1;j++){
+                if (array[j]<array[j+1]){
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
