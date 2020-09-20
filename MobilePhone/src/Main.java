0854e56f3be3fdf6bc1bf7cbffe69960480c39a5
Abu Bokor Sadik <sadikabubokor@gmail.com>
2020-09-20 22:21:36 +0600

    MobilePhone

--- MobilePhone/src/Main.java
+++ MobilePhone/src/Main.java
@@ -1,2 +1,99 @@
-package PACKAGE_NAME;public class Main {
+import com.abs.MobilePhone;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone();
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while (!quit){
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addContact();
+                    break;
+                case 3:
+                    modifyContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    searchForContact();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+
+    }
+
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of contacts.");
+        System.out.println("\t 2 - To add a contact.");
+        System.out.println("\t 3 - To modify a contact.");
+        System.out.println("\t 4 - To remove a contact.");
+        System.out.println("\t 5 - To search for a contact in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
+    public static void addContact(){
+        String name, contact;
+        System.out.print("Name: ");
+        name = scanner.nextLine();
+        System.out.print("Contact: ");
+        contact = scanner.nextLine();
+        mobilePhone.addContact(name,contact);
+    }
+
+    public static void modifyContact(){
+        String currentName, newName, newContact;
+
+        System.out.print("Current Name: ");
+        currentName = scanner.nextLine();
+        System.out.print("New Name: ");
+        newName = scanner.nextLine();
+        System.out.print("New Contact: ");
+        newContact = scanner.nextLine();
+
+        mobilePhone.modifyContact(currentName,newName,newContact);
+
+    }
+
+    public static void removeContact(){
+        String name, contact;
+        System.out.print("Name: ");
+        name = scanner.nextLine();
+        mobilePhone.removeContact(name);
+    }
+
+    public static void searchForContact(){
+        String name, contact;
+        System.out.print("Name: ");
+        name = scanner.nextLine();
+        if (mobilePhone.searchContact(name)){
+            System.out.println("Contact exist");
+        }else {
+            System.out.println("Contact dose not exist");
+        }
+    }
 }
+
+
+// Options:  Quit, print list of contacts, add new contact, update existing contact, remove contact
+// and search/find contact.
--- MobilePhone/src/com/abs/MobilePhone.java
+++ MobilePhone/src/com/abs/MobilePhone.java
@@ -1,2 +1,63 @@
-package com.abs;public class MobilePhone {
+package com.abs;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private ArrayList<Contacts> contacts = new ArrayList<Contacts>();
+
+    public void printContacts(){
+        System.out.println("Contacts ->");
+        for (int i=0;i<contacts.size();i++){
+            System.out.println(i+1 + "# Name: " + contacts.get(i).getName());
+            System.out.println("Contact: " + contacts.get(i).getPhoneNumber());
+        }
+    }
+
+    public void addContact(String name, String contact){
+        int position = findItem(name);
+        if (position>=0){
+            System.out.println("Contact exist in the list");
+        }else {
+            contacts.add(new Contacts(name,contact));
+            System.out.println("Contact information added");
+        }
+    }
+
+    public void modifyContact(String currentName, String newName, String newContact){
+        int position = findItem(currentName);
+        if (position>=0){
+            contacts.set(position,new Contacts(newName,newContact));
+            System.out.println("Contact information modified");
+        }else {
+            System.out.println("Contact dose not exist in the list");
+        }
+    }
+
+    public boolean searchContact(String name){
+
+        int position = findItem(name);
+        if (position>=0){
+            return true;
+        }
+        return false;
+    }
+
+    public void removeContact(String name){
+        int position = findItem(name);
+        if (position>=0){
+            contacts.remove(position);
+            System.out.println("Contact information deleted");
+        }else {
+            System.out.println("Contact dose not exist in the list");
+        }
+    }
+
+    private int findItem(String name){
+        for (int i=0;i<contacts.size();i++){
+            if (name.equals(contacts.get(i).getName())){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
